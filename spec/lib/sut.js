551f1b069dfc015989353430c8371b8f9c1e52b5
mileskin <miki.leskinen@reaktor.fi>
2010-12-16 22:12:55 +0200

    Modernize/cleanup by removing all semicolons (redundant nowdays).

--- spec/lib/sut.js
+++ spec/lib/sut.js
@@ -4,50 +4,50 @@ var sut = (function($) {
       $.ajax({
         url: "/questions/list",
         success: function(questions) {
-          $(".questions").html(questions);
+          $(".questions").html(questions)
         },
         error: function(xhr) {
-          $(".questions").text(xhr.responseText);
+          $(".questions").text(xhr.responseText)
         }
-      });
-    });
+      })
+    })
   }
 
   function setupAnswersBehavior() {
     $(".questions li").live("click", function() {
-      var question = $(this);
+      var question = $(this)
       $.ajax({
         url: "/answers/get?questionId=" + question.attr("id"),
         success: function(answer) {
-          $(".answerContainer").html(answer);
+          $(".answerContainer").html(answer)
           $.ajax({
             url: "/authors/get?answerId=" + $(answer).attr("id"),
             error: function(xhr) {
               $.get("/onError", function(message) {
-                $(".answerContainer .author").text(message);
-              });
+                $(".answerContainer .author").text(message)
+              })
             }
-          });
+          })
         }
-      });
-    });
+      })
+    })
   }
 
   function countResponseLength(params) {
-    var length;
+    var length
     $.ajax({
       url: params.url,
       success: function(data) {
-        length = data.length;
+        length = data.length
       },
       error: function(xhr) {
-        length = params.defaultOnError;
+        length = params.defaultOnError
         if (params.errorHandler) {
-          params.errorHandler(xhr);
+          params.errorHandler(xhr)
         }
       }
-    });
-    return length;
+    })
+    return length
   }
 
   function setupMultipleAjaxCalls() {
@@ -57,18 +57,18 @@ var sut = (function($) {
         "param1": "value1",
         "param2": "value2"
       }
-    });
-    $.get("/second?param=" + $.URLEncode(' <foo> "bar+&?#'));
-    $.get("/third", function(data){});
+    })
+    $.get("/second?param=" + $.URLEncode(' <foo> "bar+&?#'))
+    $.get("/third", function(data){})
   }
 
   function setupUserBehavior() {
     $('.showUser').click(function() {
       $.getJSON("/user", function(user) {
-        $('.user .name').text(user.name);
-        $('.user .age').text(user.age);
-      });
-    });
+        $('.user .name').text(user.name)
+        $('.user .age').text(user.age)
+      })
+    })
   }
 
   return {
@@ -77,5 +77,5 @@ var sut = (function($) {
     countResponseLength: countResponseLength,
     setupMultipleAjaxCalls: setupMultipleAjaxCalls,
     setupUserBehavior: setupUserBehavior
-  };
-})(jQuery);
+  }
+})(jQuery)
--- spec/logging-spec.js
+++ spec/logging-spec.js
@@ -1,111 +1,111 @@
 describe("logging", function() {
-  var testLog;
+  var testLog
 
   function TestLog() {
-    this.warnings = [];
-    this.errors = [];
-    this.latestWarning = function() { return this.warnings[this.warnings.length - 1]; };
-    this.latestError = function() { return this.errors[this.errors.length - 1]; };
+    this.warnings = []
+    this.errors = []
+    this.latestWarning = function() { return this.warnings[this.warnings.length - 1] }
+    this.latestError = function() { return this.errors[this.errors.length - 1] }
     jasmine.FakeAjax.log.warn = function(message) {
-      testLog.warnings.push(message);
-    };
+      testLog.warnings.push(message)
+    }
     jasmine.FakeAjax.log.error = function(message) {
-      testLog.errors.push(message);
-    };
+      testLog.errors.push(message)
+    }
   }
 
   var customMatchers = {
     toLogAndThrow: function(expectedMessage) {
-      var result = false;
-      var exception;
+      var result = false
+      var exception
       try {
-        this.actual();
+        this.actual()
       } catch (message) {
-        exception = message;
+        exception = message
       }
       if (exception) {
-        result = (this.env.equals_(exception, expectedMessage) && this.env.contains_(testLog.errors, expectedMessage));
+        result = (this.env.equals_(exception, expectedMessage) && this.env.contains_(testLog.errors, expectedMessage))
       }
       this.message = function() {
-        return "Expected function to log and throw '" + expectedMessage + "'. Logged error messages are [" + testLog.errors + "]. Actual error message thrown  by the function was '" + exception + "'.";
-      };
-      return result;
+        return "Expected function to log and throw '" + expectedMessage + "'. Logged error messages are [" + testLog.errors + "]. Actual error message thrown  by the function was '" + exception + "'."
+      }
+      return result
     }
-  };
+  }
 
   beforeEach(function() {
-    this.addMatchers(customMatchers);
-    testLog = new TestLog();
-  });
+    this.addMatchers(customMatchers)
+    testLog = new TestLog()
+  })
 
   describe("without url mappings", function() {
     beforeEach(function() {
-      $.get('/example');
-    });
+      $.get('/example')
+    })
 
     it("logs warning", function() {
-      expect(testLog.latestWarning()).toEqual("There are no ajax url mappings defined. Actual ajax url was '/example'.");
-    });
-  });
+      expect(testLog.latestWarning()).toEqual("There are no ajax url mappings defined. Actual ajax url was '/example'.")
+    })
+  })
 
   describe("without matching url", function() {
     beforeEach(function() {
-      fakeAjax({urls: {"/this": {successData: "any"}}});
-      $.get('/that', function(){});
-    });
+      fakeAjax({urls: {"/this": {successData: "any"}}})
+      $.get('/that', function(){})
+    })
 
     it("logs warning with actual url and spec description", function() {
-      expect(testLog.latestWarning()).toEqual("Applying default success data for url '/that' in spec 'logs warning with actual url and spec description'.");
-    });
-  });
+      expect(testLog.latestWarning()).toEqual("Applying default success data for url '/that' in spec 'logs warning with actual url and spec description'.")
+    })
+  })
 
   describe("without success handler in system under test", function() {
     it("logs and throws error message", function() {
       expect(function() {
-        fakeAjax({urls: {}});
-        $.get('/example');
-      }).toLogAndThrow("Ajax success handler is not defined in system under test for url '/example'. See firebug script stack for more info.");
-    });
-  });
+        fakeAjax({urls: {}})
+        $.get('/example')
+      }).toLogAndThrow("Ajax success handler is not defined in system under test for url '/example'. See firebug script stack for more info.")
+    })
+  })
 
   describe("with unknown url mapping value", function() {
     it("logs and throws error message", function() {
       expect(function() {
-        fakeAjax({urls: {"/example": 'invalid'}});
-        $.get('/example');
-      }).toLogAndThrow("Unknown mapping value for url '/example'. Expected either successData or errorMessage. Actual was 'invalid'");
-    });
-  });
+        fakeAjax({urls: {"/example": 'invalid'}})
+        $.get('/example')
+      }).toLogAndThrow("Unknown mapping value for url '/example'. Expected either successData or errorMessage. Actual was 'invalid'")
+    })
+  })
 
   describe("when fixture is not found", function() {
     it("logs and throws error message", function() {
       expect(function() {
         loadTestData('.any', 'unknown-fixture.html')
-      }).toLogAndThrow("Failed loading test data by url 'unknown-fixture.html'.");
-    });
-  });
+      }).toLogAndThrow("Failed loading test data by url 'unknown-fixture.html'.")
+    })
+  })
 
   describe("when test data is not found from fixture", function() {
     it("logs and throws error message", function() {
       expect(function() {
         loadTestData('.unknown', 'example-fixture.html')
-      }).toLogAndThrow("Failed loading test data by selector '.unknown' from url 'example-fixture.html'. Whole fixture: <html><body><div class=\"fixture\"></div></body></html>");
-    });
-  });
+      }).toLogAndThrow("Failed loading test data by selector '.unknown' from url 'example-fixture.html'. Whole fixture: <html><body><div class=\"fixture\"></div></body></html>")
+    })
+  })
 
   describe('when latest ajax is not found', function() {
     it('logs and throws error message with spec description', function() {
       expect(function() {
         latestAjax()
-      }).toLogAndThrow("Ajax hasn't yet been called in spec 'logs and throws error message with spec description'.");
-    });
-  });
+      }).toLogAndThrow("Ajax hasn't yet been called in spec 'logs and throws error message with spec description'.")
+    })
+  })
 
   describe('when matching url is not found', function() {
     it('logs and throws error message with partial url and spec description', function() {
       expect(function() {
         latestAjaxWithUrlMatching('/notfound')
-      }).toLogAndThrow("Matching url was not found by partial url '/notfound' in spec 'logs and throws error message with partial url and spec description'.");
-    });
-  });
-});
+      }).toLogAndThrow("Matching url was not found by partial url '/notfound' in spec 'logs and throws error message with partial url and spec description'.")
+    })
+  })
+})
